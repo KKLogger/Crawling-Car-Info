f35a62e501a9532b6e81bfed03bd304b4167c730
Logger <40654172+Logger@users.noreply.github.com>
2020-10-21 18:37:25 +0900

    day 7
    성능점검 까지 모두 크롤링 후 이미지로된 성능점검표 크롤링하여 다운

--- Crawling/test.py
+++ Crawling/test.py
@@ -6,6 +6,7 @@ from bs4 import BeautifulSoup as bs
 import pandas as pd
 import time
 import json
+from urllib.request import urlopen
 
 
 def get_page_url(page_num):
@@ -97,14 +98,15 @@ def get_car_info(url):
     temp.append(car_trim)
     temp.append(car_price)
     temp = temp+car_info
+    temp.append(url)
     temp.append(dealer_name)
     temp.append(dealer_location)
     temp.append(dealer_phone)
-    temp.append(url)
+
     columns = ['브랜드', '차량이름', '트림', '차량가격', '차량번호', '연식', '주행거리', '연료', '변속기', '연비', '차종', '배기량', '색상', '세금미납',
-               '압류', '저당', '제시번호', '전속이력', '침수이력', '용도이력', '소유자변경', '딜러이름', '딜러위치', '딜러전화번호', 'url']
+               '압류', '저당', '제시번호', '전속이력', '침수이력', '용도이력', '소유자변경', 'url', '딜러이름', '딜러위치', '딜러전화번호']
     df = pd.DataFrame([temp], columns=columns)
-    result = df_to_json(df)
+    result = df_to_dict(df)
     return result
 
 
@@ -161,11 +163,11 @@ def get_options(url):
                '어라운드뷰', '블랙박스', 'ABS', 'ECS', 'TCS', '차체자세제어장치', '차선이탈경보장치', '도난경보기', '타이어 공기압감지', '무릎에어백', '주차감지센서(전방)', '전방카메라', '자동긴급제동(AEB)', '스마트키', '파워윈도우', '자동도어잠금', '풀오토에어컨', '오토라이트', '자동주차시스템',
                '전자식파킹브레이크', 'HUD', 'ECM룸밀러', '크루즈컨트롤', '전동트렁크', '핸들열선', '무선도어 잠금장치', '레인센서와이퍼', '스탑앤고', '무선충전', '내비게이션', 'CD플레이어', 'CD체인저', 'AV시스템', 'MP3', 'AUX', 'USB', '핸즈프리', 'iPod 단자', '뒷좌석모니터', '스마트폰미러링', '내비게이션 (비순정)', '블루투스']
     df = pd.DataFrame([temp], columns=columns)
-    result = df_to_json(df)
+    result = df_to_dict(df)
     return result
 
 
-def df_to_json(df):
+def df_to_dict(df):
     result = df.to_dict()
     for key, value in result.items():
         result[key] = list(value.values())[0]
@@ -207,6 +209,13 @@ def get_history(url):
     soup = bs(response.text, 'lxml')
     hide_list = soup.find('ul', {'class': 'hide-list'}
                           ).find_all('span', {'class': 'txt'})
+    registeredDate = soup.find('div', {'class': 'b-right'}).find_all('tr')
+    registeredDate = registeredDate[3].find('td').text.strip()
+    noRegisterPeriod = soup.find('div', {'class', 'box-line'})
+    if noRegisterPeriod.find('div', {'class', 'date'}) is None:
+        noRegisterPeriod = "None"
+    else:
+        noRegisterPeriod = noRegisterPeriod.find('div', {'class', 'date'}).text
     result = dict()
     result['전손 보험사고'] = hide_list[0].text.strip()
     result['도난 보험사고'] = hide_list[1].text.strip()
@@ -216,6 +225,8 @@ def get_history(url):
     result['상대차 피해'] = hide_list[5].text.strip()
     result['소유자 변경'] = hide_list[6].text.strip()
     result['차량번호 변경'] = hide_list[7].text.strip()
+    result['최초보험등록날짜'] = registeredDate
+    result['미가입기간'] = noRegisterPeriod
 
     return result
 
@@ -232,6 +243,7 @@ def start():
     # print(len(car_urls))
     num = 0
     for url in car_urls:
+        print(url)
         num += 1
         print(len(car_urls), "중에", num)
         temp = dict()
@@ -242,9 +254,12 @@ def start():
 
         temp['옵션내역'] = get_options(
             url)
+        temp['성능점검'] = get_checkdata(url)
         result.append(temp)
+
     with open('./result.json', 'w', encoding='utf-8-sig') as outfile:
-        json.dump(result, outfile, indent=4, ensure_ascii=False)
+        json.dump(result, outfile, indent=4,
+                  ensure_ascii=False, sort_keys=True)
 
     # dict = {'안':'녕'}
     # with open ('j.json', 'w',encoding='utf-8-sig') as file:
@@ -252,18 +267,202 @@ def start():
     print("완-----료")
 
 
-def get_checkdata(url):
-    print(url)
-    res = requests.get(url)
+def crawl_iframe(url):
+    result = dict()
+    carSeq = url[url.index('?carSeq=') + len('?carSeq='):]
+    data = {
+        'layerId': 'layerCarCheckInfo',
+        'carSeq': carSeq
+    }
+    res = requests.post(
+        'https://www.kbchachacha.com/public/layer/car/check/info.kbc', data=data)
     soup = bs(res.text, 'lxml')
-    perform_url_ele = soup.find('a', {'id': 'btnCarCheckView1'})
-    perform_url_ele = perform_url_ele['data-link-url']
 
-    if '?' in perform_url_ele:
-        perform_domain_ele = perform_url_ele[:perform_url_ele.index('?')]
+    result = dict()
+    table = soup.find_all('table')
+    result['변속기 종류'] = table[0].find_all('tr')[6].find('div').get('value')
+    result['사용연료'] = table[0].find_all('tr')[7].find('div').get('value')
+    result['원동기형식'] = table[0].find_all('tr')[8].find('td').text
+    result['보증유형'] = table[0].find_all('tr')[9].find('div').get('value')
+    result['가격잔정 기준가격'] = table[0].find_all('tr')[10].find('td').text.strip()
+    result['주행거리 계기상태'] = table[1].find('tbody').find_all(
+        'tr')[0].find('div', {'class': 'option-ch'}).get('value')
+    result['주행거리 상태'] = table[1].find('tbody').find_all(
+        'tr')[1].find('div', {'class': 'option-ch'}).get('value')
+    result['현재 주행거리'] = table[1].find('tbody').find_all(
+        'tr')[0].find('td', {'rowspan': '2'}).text
+    result['차대번호 표기'] = table[1].find('tbody').find_all(
+        'tr')[2].find('div', {'class': 'option-ch'}).get('value')
+    result['배출가스'] = table[1].find('tbody').find_all(
+        'tr')[3].find('div', {'class': 'option-ch'}).get('value')
+    result['튜닝'] = table[1].find('tbody').find_all(
+        'tr')[4].find('div', {'class': 'option-ch'}).get('value')
+    result['사고이력'] = table[2].find('tbody').find_all('tr')[0].find_all(
+        'div', {'class': 'option-ch'})[0].get('value')
+    result['단순수리이력'] = table[2].find('tbody').find_all(
+        'tr')[0].find_all('div', {'class': 'option-ch'})[1].get('value')
+    result['외판부위 1랭크 이상여부'] = table[2].find('tbody').find_all(
+        'tr')[1].find('div', {'class': 'option-ch'}).get('value')
+    result['외판부위 2랭크 이상여부'] = table[2].find('tbody').find_all(
+        'tr')[2].find('div', {'class': 'option-ch'}).get('value')
+    result['주요골격 이상여부'] = table[2].find('tbody').find_all(
+        'tr')[3].find('div', {'class': 'option-ch'}).get('value')
+    result['자가진단 원동기'] = table[4].find('tbody').find_all(
+        'tr')[0].find('div', {'class': 'option-ch'}).get('value')
+    result['자가진단 변속기'] = table[4].find('tbody').find_all(
+        'tr')[1].find('div', {'class': 'option-ch'}).get('value')
+    result['원동기 작동상태(공회전)'] = table[4].find('tbody').find_all(
+        'tr')[2].find('div', {'class': 'option-ch'}).get('value')
+    result['원동기 오일누유 실린더커버'] = table[4].find('tbody').find_all(
+        'tr')[3].find('div', {'class': 'option-ch'}).get('value')
+    result['원동기 실린더헤드/개스킷'] = table[4].find('tbody').find_all(
+        'tr')[4].find('div', {'class': 'option-ch'}).get('value')
+    result['원동기 실린더블록/오일팬'] = table[4].find('tbody').find_all(
+        'tr')[5].find('div', {'class': 'option-ch'}).get('value')
+    result['원동기 오일 유량및 오염'] = table[4].find('tbody').find_all(
+        'tr')[6].find('div', {'class': 'option-ch'}).get('value')
+    result['원동기 냉각수누수 실린더헤드/개스킷'] = table[4].find('tbody').find_all(
+        'tr')[7].find('div', {'class': 'option-ch'}).get('value')
+    result['원동기 냉각수누수 워어펌프'] = table[4].find('tbody').find_all(
+        'tr')[8].find('div', {'class': 'option-ch'}).get('value')
+    result['원동기 냉각수누수 라디에이터'] = table[4].find('tbody').find_all(
+        'tr')[9].find('div', {'class': 'option-ch'}).get('value')
+    result['원동기 냉각수누수 냉각수 수량'] = table[4].find('tbody').find_all(
+        'tr')[10].find('div', {'class': 'option-ch'}).get('value')
+    result['원동기 고압펌프-디젤엔진'] = table[4].find('tbody').find_all(
+        'tr')[11].find('div', {'class': 'option-ch'}).get('value')
+    result['자동변속기 오일누유'] = table[4].find('tbody').find_all(
+        'tr')[12].find('div', {'class': 'option-ch'}).get('value')
+    result['자동변속기 오일유량및상세'] = table[4].find('tbody').find_all(
+        'tr')[13].find('div', {'class': 'option-ch'}).get('value')
+    result['자동변속기 작동상태(공회전)'] = table[4].find('tbody').find_all(
+        'tr')[14].find('div', {'class': 'option-ch'}).get('value')
+    result['수동변속기 오일누유'] = table[4].find('tbody').find_all(
+        'tr')[15].find('div', {'class': 'option-ch'}).get('value')
+    result['수동변속기 기어변속장치'] = table[4].find('tbody').find_all(
+        'tr')[16].find('div', {'class': 'option-ch'}).get('value')
+    result['수동변속기 오일유량및상태'] = table[4].find('tbody').find_all(
+        'tr')[17].find('div', {'class': 'option-ch'}).get('value')
+    result['수동변속기 작동상태(공회전)'] = table[4].find('tbody').find_all(
+        'tr')[18].find('div', {'class': 'option-ch'}).get('value')
+    result['동력전달 클러치어셈블러'] = table[4].find('tbody').find_all(
+        'tr')[19].find('div', {'class': 'option-ch'}).get('value')
+    result['동력전달 등속조인트'] = table[4].find('tbody').find_all(
+        'tr')[20].find('div', {'class': 'option-ch'}).get('value')
+    result['동력전달 추진축및베어링'] = table[4].find('tbody').find_all(
+        'tr')[21].find('div', {'class': 'option-ch'}).get('value')
+    result['동력전달 디퍼렌셜기어'] = table[4].find('tbody').find_all(
+        'tr')[22].find('div', {'class': 'option-ch'}).get('value')
+    result['조향 동력조향작동오일노유'] = table[4].find('tbody').find_all(
+        'tr')[23].find('div', {'class': 'option-ch'}).get('value')
+    result['조향 작동상태 스티어링펌프'] = table[4].find('tbody').find_all(
+        'tr')[24].find('div', {'class': 'option-ch'}).get('value')
+    result['조향 작동상태 스티어링기어'] = table[4].find('tbody').find_all(
+        'tr')[25].find('div', {'class': 'option-ch'}).get('value')
+    result['조향 작동상태 스티어링조인트'] = table[4].find('tbody').find_all(
+        'tr')[26].find('div', {'class': 'option-ch'}).get('value')
+    result['조향 작동상태 파워고압호스'] = table[4].find('tbody').find_all(
+        'tr')[27].find('div', {'class': 'option-ch'}).get('value')
+    result['조향 작동상태 타이로드엔드및볼조인트'] = table[4].find('tbody').find_all(
+        'tr')[28].find('div', {'class': 'option-ch'}).get('value')
+    result['제동 브레이크마스터실린더오일누유'] = table[4].find('tbody').find_all(
+        'tr')[29].find('div', {'class': 'option-ch'}).get('value')
+    result['제동 브레이크오일누유'] = table[4].find('tbody').find_all(
+        'tr')[30].find('div', {'class': 'option-ch'}).get('value')
+    result['제동 배력장치상태'] = table[4].find('tbody').find_all(
+        'tr')[31].find('div', {'class': 'option-ch'}).get('value')
+    result['전기 발전기출력'] = table[4].find('tbody').find_all(
+        'tr')[32].find('div', {'class': 'option-ch'}).get('value')
+    result['전기 시동모터'] = table[4].find('tbody').find_all(
+        'tr')[33].find('div', {'class': 'option-ch'}).get('value')
+    result['전기 와이퍼모터기능'] = table[4].find('tbody').find_all(
+        'tr')[34].find('div', {'class': 'option-ch'}).get('value')
+    result['전기 실내송풍모터'] = table[4].find('tbody').find_all(
+        'tr')[35].find('div', {'class': 'option-ch'}).get('value')
+    result['전기 라디에이터팬모터'] = table[4].find('tbody').find_all(
+        'tr')[36].find('div', {'class': 'option-ch'}).get('value')
+    result['전기 윈도우모터작동'] = table[4].find('tbody').find_all(
+        'tr')[37].find('div', {'class': 'option-ch'}).get('value')
+    result['고전원전기장치 충전구절연상태'] = table[4].find('tbody').find_all(
+        'tr')[38].find('div', {'class': 'option-ch'}).get('value')
+    result['고전원전기장치 구동축전지격리상태'] = table[4].find('tbody').find_all(
+        'tr')[39].find('div', {'class': 'option-ch'}).get('value')
+    result['고전원전기장치 고전원전기배선상태'] = table[4].find('tbody').find_all(
+        'tr')[40].find('div', {'class': 'option-ch'}).get('value')
+    result['연료 연료누출'] = table[4].find('tbody').find_all(
+        'tr')[41].find('div', {'class': 'option-ch'}).get('value')
+    # result['수리필요 외장'] = table[5].find('tbody').find_all(
+    #     'tr')[0].find('div', {'class': 'option-ch'}).get('value')
+    # result['수리필요 광택'] = table[5].find('tbody').find_all(
+    #     'tr')[1].find('div', {'class': 'option-ch'}).get('value')
+    # result['수리필요 내장'] = table[5].find('tbody').find_all(
+    #     'tr')[2].find('div', {'class': 'option-ch'}).get('value')
+    # result['수리필요 룸크리닝'] = table[5].find('tbody').find_all(
+    #     'tr')[3].find('div', {'class': 'option-ch'}).get('value')
+    # result['수리필요 휠'] = table[5].find('tbody').find_all(
+    #     'tr')[4].find('div', {'class': 'option-ch'}).get('value')
+    # result['수리필요 타이어'] = table[5].find('tbody').find_all(
+    #     'tr')[5].find('div', {'class': 'option-ch'}).get('value')
+    # result['수리필요 유리'] = table[5].find('tbody').find_all(
+    #     'tr')[6].find('div', {'class': 'option-ch'}).get('value')
+    # result['기본품목 보유상태'] = table[5].find('tbody').find_all(
+    #     'tr')[7].find('div', {'class': 'option-ch'}).get('value')
+    result['성능점검일'] = soup.find('div', {'class': 'date'}).text
+
+    return result
+
+
+def get_checkdata(url):
+    headers = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive',
+        'Host': 'www.kbchachacha.com',
+        'Pragma': 'no-cache',
+        'Sec-Fetch-Dest': 'document',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-Site': 'none',
+        'Sec-Fetch-User': '?1',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36'
+    }
+    res = requests.get(
+        url, headers=headers)
+    soup = bs(res.text, 'lxml')
+    chk_tag_url = soup.find('li', {'class': 'used01'}).find('a')[
+        'data-link-url']
+    if 'http' in chk_tag_url:  # 다른 페이지로 이동
+        # call domain by scraper
+        result = "other page"
     else:
-        perform_domain_ele = perform_url_ele
-    print(perform_url_ele)
+        carSeq = url[url.index('?carSeq=')+len('?carSeq='):]
+        data = {
+            'layerId': 'layerCarCheckInfo',
+            'carSeq': carSeq
+        }
+        res = requests.post(
+            'https://www.kbchachacha.com/public/layer/car/check/info.kbc', data=data)
+
+        soup = bs(res.text, 'lxml')
+
+        img_check = soup.find('div', {'class': 'ch-car-txt'})
+        if img_check == None:
+            if soup.find('div', {'class': 'ch-car-name'}) == None:
+                result = "no data"
+                # print("None Data")
+                pass
+            else:
+
+                # call iframe scraper
+                result = crawl_iframe(url)
+        else:
+            # print("image")
+            get_image(url)
+            result = "image"
+
+    return result
 
     # 성능점검 url 구하기
     # urls = get_car_urls()
@@ -285,4 +484,25 @@ def get_checkdata(url):
     #     result.append([perform_url_ele, perform_domain_ele])
     # df = pd.DataFrame(result)
     # df.to_csv('성능점검urls.csv', sep=',', encoding='euc-kr', index=False)
+
+
+def get_image(url):
+    carSeq = url[url.index('?carSeq=') + len('?carSeq='):]
+    data = {
+        'layerId': 'layerCarCheckInfo',
+        'carSeq': carSeq
+    }
+    res = requests.post(
+        'https://www.kbchachacha.com/public/layer/car/check/info.kbc', data=data)
+    soup = bs(res.text, 'lxml')
+    title = soup.find('h2').text.split(' ')[1]
+    img_urls = soup.find_all('img')
+    img_urls = [x.get('src') for x in img_urls]
+    for num, img_url in enumerate(img_urls):
+        with urlopen(img_url) as f:
+            with open('image/' + str(num) + title + '.jpg', 'wb') as h:
+                img = f.read()
+                h.write(img)
+
+
 start()
