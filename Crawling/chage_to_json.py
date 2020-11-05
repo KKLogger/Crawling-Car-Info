dff56994470e6e3769811817f099d3493463d560
Logger <40654172+Logger@users.noreply.github.com>
2020-11-05 23:59:40 +0900

    day 13 error2
    josn 파일 형식 이상 -> 수정

--- Crawling/chage_to_json.py
+++ Crawling/chage_to_json.py
@@ -24,7 +24,6 @@ for str_data in str_datas:
         json_data = json.loads(str_data)
         result.append(dict_data)
     except:
-        print(str_data)
         print("Fail", num)
 
 
@@ -47,5 +46,45 @@ for str_data in str_datas:
 
 # json_data = json.loads(str_data, encoding='utf-8')
 print(len(result))
-with open('C:/Users/jlee/Desktop/result1.json', 'w', encoding='utf-8-sig') as ff:
-    json.dump(result, ff, indent=4, ensure_ascii=False, sort_keys=True)
+
+with open('C:/Users/jlee/Desktop/result1.json'.format(num=num), 'w', encoding='utf-8-sig') as ff:
+    json.dump(result[:10000], ff, indent=4, ensure_ascii=False, sort_keys=True)
+with open('C:/Users/jlee/Desktop/result2.json'.format(num=num), 'w', encoding='utf-8-sig') as ff:
+    json.dump(result[10000:20000], ff, indent=4,
+              ensure_ascii=False, sort_keys=True)
+with open('C:/Users/jlee/Desktop/result3.json'.format(num=num), 'w', encoding='utf-8-sig') as ff:
+    json.dump(result[20000:30000], ff, indent=4,
+              ensure_ascii=False, sort_keys=True)
+with open('C:/Users/jlee/Desktop/result4.json'.format(num=num), 'w', encoding='utf-8-sig') as ff:
+    json.dump(result[30000:40000], ff, indent=4,
+              ensure_ascii=False, sort_keys=True)
+with open('C:/Users/jlee/Desktop/result5.json'.format(num=num), 'w', encoding='utf-8-sig') as ff:
+    json.dump(result[40000:50000], ff, indent=4,
+              ensure_ascii=False, sort_keys=True)
+with open('C:/Users/jlee/Desktop/result6.json'.format(num=num), 'w', encoding='utf-8-sig') as ff:
+    json.dump(result[50000:60000], ff, indent=4,
+              ensure_ascii=False, sort_keys=True)
+with open('C:/Users/jlee/Desktop/result7.json'.format(num=num), 'w', encoding='utf-8-sig') as ff:
+    json.dump(result[60000:70000], ff, indent=4,
+              ensure_ascii=False, sort_keys=True)
+with open('C:/Users/jlee/Desktop/result8.json'.format(num=num), 'w', encoding='utf-8-sig') as ff:
+    json.dump(result[70000:80000], ff, indent=4,
+              ensure_ascii=False, sort_keys=True)
+with open('C:/Users/jlee/Desktop/result9.json'.format(num=num), 'w', encoding='utf-8-sig') as ff:
+    json.dump(result[80000:90000], ff, indent=4,
+              ensure_ascii=False, sort_keys=True)
+with open('C:/Users/jlee/Desktop/result10.json'.format(num=num), 'w', encoding='utf-8-sig') as ff:
+    json.dump(result[90000:100000], ff, indent=4,
+              ensure_ascii=False, sort_keys=True)
+with open('C:/Users/jlee/Desktop/result11.json'.format(num=num), 'w', encoding='utf-8-sig') as ff:
+    json.dump(result[100000:110000], ff, indent=4,
+              ensure_ascii=False, sort_keys=True)
+with open('C:/Users/jlee/Desktop/result12.json'.format(num=num), 'w', encoding='utf-8-sig') as ff:
+    json.dump(result[110000:120000], ff, indent=4,
+              ensure_ascii=False, sort_keys=True)
+with open('C:/Users/jlee/Desktop/result13.json'.format(num=num), 'w', encoding='utf-8-sig') as ff:
+    json.dump(result[120000:130000], ff, indent=4,
+              ensure_ascii=False, sort_keys=True)
+with open('C:/Users/jlee/Desktop/result14.json'.format(num=num), 'w', encoding='utf-8-sig') as ff:
+    json.dump(result[130000:], ff, indent=4,
+              ensure_ascii=False, sort_keys=True)
