b32472849195ae3d0f659c3620dfbe2d65f4f5c9
Logger <40654172+Logger@users.noreply.github.com>
2020-11-13 11:42:46 +0900

    final

--- Crawling/to_server/ttt.py
+++ Crawling/to_server/ttt.py
@@ -6,8 +6,9 @@ import os
 def process_json():
 
     result = list()
-    for num in range(1, 51):
-        with open('C:/Users/jlee/Desktop/result{num}_t.json'.format(num=num), encoding='utf-8-sig', errors='ignore') as f:
+    for num in range(1, 50):
+        print(num)
+        with open('C:/Users/jlee/Desktop/AWS/result{num}_t.json'.format(num=num), encoding='utf-8-sig', errors='ignore') as f:
             str_data = f.read()
         str_data = str(str_data)
         str_data = str_data[:]
@@ -15,11 +16,10 @@ def process_json():
         str_data = str_data.replace('}{', "}///{")
         str_datas = str_data.split('///')
         str_datas = [x.replace("'", '"') for x in str_datas]
-        num = 0
-
+        num_s = 0
+        str_datas = list(set(str_datas))
         for str_data in str_datas:
-            num += 1
-
+            num_s += 1
             try:
                 dict_data = literal_eval(str_data)
                 json_data = json.loads(str_data)
@@ -27,30 +27,29 @@ def process_json():
                     dict_data['noRegisterPeriod'] = 'null'
                 result.append(dict_data)
             except:
-                print("Fail", num)
-    result = list(set(result))
+                print("Fail", num_s)
 
     print("총 json에 차량 개수 ", len(result))
 
-    with open('result.json', 'w', encoding='utf-8-sig') as ff:
+    with open('C:/Users/jlee/Desktop/AWS/result.json', 'w', encoding='utf-8-sig') as ff:
         json.dump(result, ff, indent=4, ensure_ascii=False, sort_keys=True)
 
 
 ###main ##
-# process_json()
+process_json()
 
-for num in range(32, 49):
-    result = list()
-    with open('C:/Users/jlee/Desktop/result{num}_t.json'.format(num=num), encoding='utf-8-sig', errors='ignore') as f:
-        str_data = f.read()
-    str_data = str_data.replace('{}', "")
-    str_data = str_data.replace('}{', "}///{")
-    str_datas = str_data.split('///')
+# for num in range(32, 49):
+#     result = list()
+#     with open('C:/Users/jlee/Desktop/result{num}_t.json'.format(num=num), encoding='utf-8-sig', errors='ignore') as f:
+#         str_data = f.read()
+#     str_data = str_data.replace('{}', "")
+#     str_data = str_data.replace('}{', "}///{")
+#     str_datas = str_data.split('///')
 
-    for num, str_data in enumerate(str_datas):
-        if num == 499:
-            break
-        dict_data = literal_eval(str_data)
-        with open('C:/Users/jlee/Desktop/result50_t.json', 'a', encoding='utf-8-sig') as ff:
-            json.dump(dict_data, ff, indent=4,
-                      ensure_ascii=False, sort_keys=True)
+#     for num, str_data in enumerate(str_datas):
+#         if num == 499:
+#             break
+#         dict_data = literal_eval(str_data)
+#         with open('C:/Users/jlee/Desktop/result50_t.json', 'a', encoding='utf-8-sig') as ff:
+#             json.dump(dict_data, ff, indent=4,
+#                       ensure_ascii=False, sort_keys=True)
