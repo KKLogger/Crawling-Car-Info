b0b5145216b9210f44cc90434a4560dcc6c851c7
Logger <40654172+Logger@users.noreply.github.com>
2020-11-20 10:18:41 +0900

    SSH connect FTP

--- Crawling/to_server/merge_json.py
+++ Crawling/to_server/merge_json.py
@@ -8,7 +8,7 @@ import sys
 '''
 # local_path = "/home/centos/result_from_servers/"
 
-local_path = "C:/Users/jlee/Desktop/아름드리/BackUp/1119/"
+local_path = "C:/Users/jlee/Desktop/아름드리/BackUp/1120/"
 day = datetime.today().strftime('%Y%m%d')
 if __name__ == '__main__':
     result = list()
